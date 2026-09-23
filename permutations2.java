a29457afb9cb41538c771ece11944880e300081c
ranjeet <ranjeet@example.com>
2026-09-23 18:57:47 +0530

    commit

---
+++ permutations2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class permutations2 {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>());
+
+        return ans;
+    }
+
+    void backtrack(int[] nums, boolean[] used, List<Integer> temp) {
+
+        if (temp.size() == nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i])
+                continue;
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            backtrack(nums, used, temp);
+
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
